44a62714a7ddc541d8a814690ecb9332aa2150f5
mishhhhhhhh17 <michellezhang11182@gmail.com>
2022-09-30 15:24:06 -0500

    wrote some basic tests in BucketList.java

--- .github/src/Java/models/BucketList.java
+++ .github/src/Java/models/BucketList.java
@@ -3,13 +3,26 @@ import java.util.ArrayList;
 
 public class BucketList {
     private ArrayList<String> b_List;
+
+    @Test
     public BucketList() {
         b_List = new ArrayList<>();
+        assertTrue(b_list.empty());
     }
+
+    @Test
     public void addItem(String item) {
+        int initial_size = b_list.size();
         b_List.add(item);
+        int final_size = b_list.size();
+        assertTrue(initial_size < final_size);
     }
+
+    @Test
     public bool removeItem(String item) {
+        int initial_size = b_list.size();
         return b_List.remove(item);
+        int final_size = b_list.size();
+        assertTrue(initial_size > final_size);
     }
 }
