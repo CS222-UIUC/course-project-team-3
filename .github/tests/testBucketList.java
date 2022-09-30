e9d22568a130df8ef96f60e23334112b80f30458
Noah Carroll <noahlc2@illinois.edu>
2022-09-30 16:15:54 -0500

    added private variable b_length for bucketList and made a test case for BucketList

--- .github/tests/testBucketList.java
+++ .github/tests/testBucketList.java
@@ -1,5 +1,9 @@
 import src.Java.models.BucketList.java; // dont know if this is correct, just copied from the video
 
 public class testBucketList {
-
+    BucketList test = new BucketList();
+    test.addItem("Fly");
+    test.addItem("Travel");
+    test.addItem("Code");
+    assert (test.b_length == 3);
 }
