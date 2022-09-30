12cb1ecf9b15c3e0a181e8f3a236c19791f876fa
Noah Carroll <noahlc2@illinois.edu>
2022-09-30 16:15:54 -0500

    added private variable b_length for bucketList and made a test case for BucketList

--- .github/src/Java/models/BucketList.java
+++ .github/src/Java/models/BucketList.java
@@ -6,10 +6,12 @@ import static org.junit.Assert.assertTrue;
 
 public class BucketList {
     private ArrayList<String> b_List;
+    private int b_length;
 
     @Test
     public BucketList() {
         b_List = new ArrayList<>();
+        b_length = 0;
         assertTrue(b_list.empty());
     }
 
@@ -17,6 +19,7 @@ public class BucketList {
     public void addItem(String item) {
         int initial_size = b_list.size();
         b_List.add(item);
+        b_length++;
         int final_size = b_list.size();
         assertTrue(initial_size < final_size);
     }
@@ -24,6 +27,7 @@ public class BucketList {
     @Test
     public bool removeItem(String item) {
         int initial_size = b_list.size();
+        b_length--;
         return b_List.remove(item);
         int final_size = b_list.size();
         assertTrue(initial_size > final_size);
