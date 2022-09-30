506346a3a56235c7198c2203489d590e4bd44ebe
jasonwu5illini <92773528+jasonwu5illini@users.noreply.github.com>
2022-09-30 14:49:19 -0500

    very basic bucketlist class

--- .github/src/Java/models/BucketList.java
+++ .github/src/Java/models/BucketList.java
@@ -1,1 +1,15 @@
 
+import java.util.ArrayList;
+
+public class BucketList {
+    private ArrayList<String> b_List;
+    public BucketList() {
+        b_List = new ArrayList<>();
+    }
+    public void addItem(String item) {
+        b_List.add(item);
+    }
+    public bool removeItem(String item) {
+        return b_List.remove(item);
+    }
+}
