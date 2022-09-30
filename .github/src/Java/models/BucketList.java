51d4356fdf3e4410d42e9590acdaf09786ec89d7
mishhhhhhhh17 <michellezhang11182@gmail.com>
2022-09-30 15:37:08 -0500

    edited basic tests in bucketlist.java

--- .github/src/Java/models/BucketList.java
+++ .github/src/Java/models/BucketList.java
@@ -5,6 +5,9 @@ import org.junit.Test;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public class BucketList {
     private ArrayList<String> b_List;
     @Test
